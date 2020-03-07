6c479ad6fc94b5c6aab4e2b2c641ce5893a0d807
johnboggess <johnboggess@southern.edu>
2020-03-07 12:02:10 -0500

    Parsed literal can be compiled into literal nodes, addedd tests

---
+++ BogieLang/Runtime/Nodes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BogieLang.Runtime
+{
+    public class Literal
+    {
+        public double? Real = null;
+        public int? Integer = null;
+        public bool? Bool = null;
+        public string String = null;
+
+        public static Literal Compile(BogieLangParser.LiteralContext literalContext)
+        {
+            Literal result = new Literal();
+            if(literalContext.REAL() != null) { result.Real = double.Parse(literalContext.REAL().GetText()); }
+            else if (literalContext.INTEGER() != null) { result.Integer = int.Parse(literalContext.INTEGER().GetText()); }
+            else if (literalContext.BOOL() != null) { result.Bool = bool.Parse(literalContext.BOOL().GetText()); }
+            else if (literalContext.STRING() != null)
+            {
+                result.String = literalContext.STRING().GetText();
+                result.String = result.String.Remove(0, 1);
+                result.String = result.String.Remove(result.String.Length-1, 1);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BogieLangTests/CompileTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Antlr4.Runtime;
+
+using BogieLang;
+using BogieLang.Runtime;
+namespace BogieLangTests
+{
+    class CompileTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LiteralTests()
+        {
+            string txt = "1";
+            AntlrInputStream inputStream = new AntlrInputStream(txt);
+            BogieLangLexer lexer = new BogieLangLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+            BogieLangParser parser = new BogieLangParser(commonTokenStream);
+            BogieLangParser.LiteralContext literalContext = parser.literal();
+            BogieLangBaseVisitor<object> visitor = new BogieLangBaseVisitor<object>();
+            visitor.Visit(literalContext);
+            Literal literal = Literal.Compile(literalContext);
+            Assert.True(literal.Real == null);
+            Assert.True(literal.Integer == 1);
+            Assert.True(literal.Bool == null);
+            Assert.True(literal.String == null);
+
+
+            txt = "1.0";
+            inputStream = new AntlrInputStream(txt);
+            lexer = new BogieLangLexer(inputStream);
+            lexer.AddErrorListener(new ParserErrorHandler<int>());
+            commonTokenStream = new CommonTokenStream(lexer);
+            parser = new BogieLangParser(commonTokenStream);
+            parser.AddErrorListener(new ParserErrorHandler<object>());
+            literalContext = parser.literal();
+            visitor = new BogieLangBaseVisitor<object>();
+            visitor.Visit(literalContext);
+            literal = Literal.Compile(literalContext);
+            Assert.True(literal.Real == 1.0);
+            Assert.True(literal.Integer == null);
+            Assert.True(literal.Bool == null);
+            Assert.True(literal.String == null);
+
+
+            txt = "false";
+            inputStream = new AntlrInputStream(txt);
+            lexer = new BogieLangLexer(inputStream);
+            lexer.AddErrorListener(new ParserErrorHandler<int>());
+            commonTokenStream = new CommonTokenStream(lexer);
+            parser = new BogieLangParser(commonTokenStream);
+            parser.AddErrorListener(new ParserErrorHandler<object>());
+            literalContext = parser.literal();
+            visitor = new BogieLangBaseVisitor<object>();
+            visitor.Visit(literalContext);
+            Assert.True(parser.NumberOfSyntaxErrors == 0);
+            literal = Literal.Compile(literalContext);
+            Assert.True(literal.Real == null);
+            Assert.True(literal.Integer == null);
+            Assert.True(literal.Bool == false);
+            Assert.True(literal.String == null);
+
+
+            txt = "\"asd8 99asd\"";
+            inputStream = new AntlrInputStream(txt);
+            lexer = new BogieLangLexer(inputStream);
+            lexer.AddErrorListener(new ParserErrorHandler<int>());
+            commonTokenStream = new CommonTokenStream(lexer);
+            parser = new BogieLangParser(commonTokenStream);
+            parser.AddErrorListener(new ParserErrorHandler<object>());
+            literalContext = parser.literal();
+            visitor = new BogieLangBaseVisitor<object>();
+            visitor.Visit(literalContext);
+            Assert.True(parser.NumberOfSyntaxErrors == 0);
+            literal = Literal.Compile(literalContext);
+            Assert.True(literal.Real == null);
+            Assert.True(literal.Integer == null);
+            Assert.True(literal.Bool == null);
+            Assert.True(literal.String == "asd8 99asd");
+        }
+    }
+}
