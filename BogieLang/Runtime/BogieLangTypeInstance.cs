61ab1aed3641d5c1c5b4e0e9e845a034ac29c90a
johnboggess <johnboggess@southern.edu>
2020-03-09 22:51:37 -0400

    Added BogieLangTypeInstance to represent specific instances of a type, added RuntimeEnvironment for holding function definitions and global variables

---
+++ BogieLang/Runtime/BogieLangTypeInstance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BogieLang.Runtime
+{
+    public class BogieLangTypeInstance
+    {
+        public string Identifer = null;
+        public BogieLangType? BogieLangType = null;
+        public object Value;
+    }
+}
---
+++ BogieLang/Runtime/RuntimeEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BogieLang.Runtime
+{
+    using VariableEnvironment = Dictionary<string, BogieLangTypeInstance>;
+
+    public class RuntimeEnvironment
+    {
+        public VariableEnvironment GlobalVariables = new VariableEnvironment();
+        public Dictionary<string, FunctionDefinition> FunctionDefinitions = new Dictionary<string, FunctionDefinition>();
+    }
+}
